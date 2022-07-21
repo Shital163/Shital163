a9fa7399e6c4f4eae866dd8032cb5ac3b5cbfd5b
Dongare Shital Sudhakar <DO953202@wipro.com>
2022-07-21 14:53:40 +0530

    test build using ultrahook

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld{
      System.out.println("Hello Java");  
      System.out.println("Build triggered automatically after push event!");
      System.out.println("Build triggered automatically after push event2!");
+     System.out.println("Build triggered automatically after push event3... webhook using ultrahook");
     }  
 }  
